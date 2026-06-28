0683efdde522a37cbe219a98ada47ba466c0e16c
Arvind <120897974+arvindfroi@users.noreply.github.com>
2026-06-28 04:29:12 +0200

    Add Norwegian Nynorsk (nn) locale

---
+++ src/locale/nn.js
@@ -0,0 +1,12 @@
+export default {
+    days: ["Søndag", "Måndag", "Tysdag", "Onsdag", "Torsdag", "Fredag", "Laurdag"],
+    daysShort: ["Søn", "Mån", "Tys", "Ons", "Tor", "Fre", "Lau"],
+    daysMin: ["Sø", "Må", "Ty", "On", "To", "Fr", "La"],
+    months: ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
+    today: "I dag",
+    clear: "Fjern",
+    dateFormat: "dd.mm.yyyy",
+    timeFormat: "HH:mm",
+    firstDay: 1
+};
